605e3d4dde4c1b4e03f2e54a8b21b33c164eef63
Harim <godtoether94@naver.com>
2022-07-01 16:49:33 +0900

    22.07.01 하림

--- src/com/itwill/marketcoli/김하림/OrderDetail.java
+++ src/com/itwill/marketcoli/김하림/OrderDetail.java
@@ -1,4 +1,7 @@
 package com.itwill.marketcoli.김하림;
+
+import com.itwill.marketcoli.dto.Orders;
+
 /*
 orderdetail(
 		od_no   NUMBER(10)		 NULL ,
@@ -11,23 +14,25 @@ orderdetail(
  * @author ITWILL
  *
  */
+/**
+ * @author ITWILL
+ *
+ */
 public class OrderDetail {
 	private int od_no;		//주문상세번호
-	private Oreders order;
-	private int p_piece;	//주문상품갯수
-	private int u_no;		//회원 번호
-	private int p_no;		//상품 번호
+	private Orders order;
+	//private int p_piece;	//주문상품갯수
+	//private int u_no;		//회원 번호
+	//private int p_no;		//상품 번호
 	
 	 public OrderDetail() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public OrderDetail(int od_no, int p_piece, int u_no, int p_no) {
+	public OrderDetail(int od_no, Orders order) {
 		super();
 		this.od_no = od_no;
-		this.p_piece = p_piece;
-		this.u_no = u_no;
-		this.p_no = p_no;
+		this.order = order;
 	}
 
 	public int getOd_no() {
@@ -38,34 +43,20 @@ public class OrderDetail {
 		this.od_no = od_no;
 	}
 
-	public int getP_piece() {
-		return p_piece;
+	public Orders getOrder() {
+		return order;
 	}
 
-	public void setP_piece(int p_piece) {
-		this.p_piece = p_piece;
-	}
-
-	public int getU_no() {
-		return u_no;
-	}
-
-	public void setU_no(int u_no) {
-		this.u_no = u_no;
-	}
-
-	public int getP_no() {
-		return p_no;
-	}
-
-	public void setP_no(int p_no) {
-		this.p_no = p_no;
+	public void setOrder(Orders order) {
+		this.order = order;
 	}
 
 	@Override
 	public String toString() {
-		return "OrderDetail [od_no=" + od_no + ", p_piece=" + p_piece + ", u_no=" + u_no + ", p_no=" + p_no + "]";
+		return "OrderDetail [od_no=" + od_no + ", order=" + order + "]";
 	}
+
+	
 	
 	
 	
--- src/com/itwill/marketcoli/김하림/OrderDetailDao.java
+++ src/com/itwill/marketcoli/김하림/OrderDetailDao.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 
 import com.itwill.marketcoli.common.DataSource;
 import com.itwill.marketcoli.dto.OrderDetail;
+import com.itwill.marketcoli.dto.Orders;
+import com.itwill.marketcoli.dto.UserInfo;
 
 public class OrderDetailDao {
 	private DataSource dataSource;
@@ -12,11 +14,26 @@ public class OrderDetailDao {
 	public OrderDetailDao() {
 		dataSource = new DataSource();
 	}
-
-	public int insertOrderDetail(OrderDetail orderdetail) throws Exception  {
-		Connection con = dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(OrderDetailSQL.ORDERDETAIL_INSERT);
+	/*
+	private int od_no;		//주문상세번호
+	private Orders order;
+	*/
+	
+	//주문상세 내역 확인
+	public int SelectOrderDetail(UserInfo u_id) {
 		return 0;
+		
 	}
-	}
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
 	
