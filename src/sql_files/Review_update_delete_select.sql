bee62628444e0dd04020133d39db6baa1a70709f
seoyoung <seoyoung310@gmail.com>
2022-07-01 20:54:06 +0900

    review sql 수정

--- src/sql_files/Review_update_delete_select.sql
+++ src/sql_files/Review_update_delete_select.sql
@@ -1,5 +1,5 @@
 --Review update
-update review set r_image = 'rr1.jpg', r_content='너무너무 맛있어요 최고!', r_rating='4' where u_no = 1; 
+update review set r_image = 'ff.jpg', r_content='너무 맛있어요 최고!', r_rating='4' where u_no = 4; 
 
 --Review delete
 delete review where u_no = 1;
