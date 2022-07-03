faeb631c25433b315754e20dd58fa6b0d37a09a3
Harim <godto@DESKTOP-5CJT6FD>
2022-07-04 00:58:38 +0900

    22.07.04 하림

--- com/itwill/marketcoli/김하림/OrderDetailDao.java
+++ com/itwill/marketcoli/김하림/OrderDetailDao.java
@@ -23,17 +23,8 @@ public class OrderDetailDao {
 	//주문상세 내역 확인 -유저아이디로 확인
 	
 	
-	//주문상세 내역 상품 수량 변경 - update
-//	public int update(int p_piece,int u_no,int p_no) throws Exception {
-//		String ORDERDETAIL_UPDATE = "update orderdetail set p_piece =? where u_no=? and p_no=?";
-//		Connection con =dataSource.getConnection();
-//		PreparedStatement pstmt = con.prepareStatement(ORDERDETAIL_UPDATE);
-//		pstmt.setInt(1, p_piece);
-//		pstmt.setInt(2, u_no);
-//		pstmt.setInt(3, p_no);
-//		int rowCount = pstmt.executeUpdate();
-//		return rowCount;		
-//	}
+
+	//수량 변경(update)-유저번호를 이용하여 조회후 수량 변경 
 	public int updateOrderByno(OrderDetail orderDetail) throws Exception {
 		String ORDERDETAIL_UPDATE = "update orderdetail set p_piece =? where p_no=? and  u_no=?";
 		Connection con = this.dataSource.getConnection();
@@ -44,14 +35,14 @@ public class OrderDetailDao {
 		int rowCount = pstmt.executeUpdate();
 		return rowCount;		
 	}
-	
-	public int update(OrderDetail orderDetail) throws Exception {
-		String ORDERDETAIL_UPDATE = "update orderdetail set p_piece =? where p_no=? and  u_no=?";
+	//ㅅ
+	public int updateOrderByid(OrderDetail orderDetail) throws Exception {
+		String ORDERDETAIL_UPDATE = "update orderdetail set p_piece =? where p_no=? and  u_id=?";
 		Connection con = this.dataSource.getConnection();
 		PreparedStatement pstmt = con.prepareStatement(ORDERDETAIL_UPDATE);
 		pstmt.setInt(1,orderDetail.getOrder().getProduct().getP_no());
 		pstmt.setInt(2, orderDetail.getOrder().getProduct().getP_no());
-		pstmt.setInt(3, orderDetail.getOrder().getUserInfo().getU_no());
+		pstmt.setString(3, orderDetail.getOrder().getUserInfo().getU_id());
 		int rowCount = pstmt.executeUpdate();
 		return rowCount;		
 	}
@@ -60,15 +51,15 @@ public class OrderDetailDao {
 	
 	
 	
-	public List<OrderDetail> selectbyid(String u_id) throws Exception {	
+	public List<OrderDetail> selectbyu_id(String u_id) throws Exception {	
 		String ORDERDETAIL_SELECT_BY_id = "select * from orderdetail where u_id=?";
-		List<OrderDetail> orderDetailsList = new ArrayList<OrderDetail>();
+		List<OrderDetail> orderDetailList = new ArrayList<OrderDetail>();
 		Connection con = this.dataSource.getConnection();
 		PreparedStatement pstmt = con.prepareStatement(ORDERDETAIL_SELECT_BY_id);
 		pstmt.setString(1, u_id);
-		ResultSet rs = pstmt.executeQuery();
+		ResultSet rs= pstmt.executeQuery();
 		while (rs.next()) {
-			orderDetailsList.add(new OrderDetail(0,
+			orderDetailList.add(new OrderDetail(0,
 								 new Orders(0, null, null, 0, 0, null, 
 								 new UserInfo(0, rs.getString("u_id"), null, null, null, 0, null, 0, null, null))));
 	
@@ -76,7 +67,7 @@ public class OrderDetailDao {
 		rs.close();
 		pstmt.close();
 		con.close();
-		return orderDetailsList;
+		return orderDetailList;
 		
 		
 		
@@ -89,19 +80,7 @@ public class OrderDetailDao {
 	
 	
 	
-		
-	//주문상세 내역 - 생일 인자를 이용하여 검색 -> 인자 수정예정	
-	public int selectByBirth(int u_birth) throws Exception	{
-		String ORDERDETAIL_SELECT_BY_phone = "select * from orderdetail od join UserInfo u on od.u_no = u.u_no where u_birth=? ";
-		Connection con =dataSource.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(ORDERDETAIL_SELECT_BY_phone);
-		pstmt.setInt(1, u_birth);
-		int rowCount = pstmt.executeUpdate();
-		return rowCount;
-		
-	}
-	
-	
+
 }
 	
 	
--- com/itwill/marketcoli/김하림/OrderDetailDaoTestMain.java
+++ com/itwill/marketcoli/김하림/OrderDetailDaoTestMain.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.itwill.marketcoli.dto.Product;
 import com.itwill.marketcoli.dto.UserInfo;
+import com.itwill.marketcoli.최서영.Review;
 
 public class OrderDetailDaoTestMain {
 
@@ -13,32 +14,23 @@ public class OrderDetailDaoTestMain {
 		
 		System.out.println("1.유저번호를 이용한 상품수량 변경"); //유저 번호와 상품번호를 찾아서 상품 수량 변경
 		OrderDetail updateorderDetail_u_no =	new OrderDetail(0,new Orders(0, null, null, 0, 7,
-										new Product(3, null, 0, 0, null, null, null),
-										new UserInfo(2, null, null, null, null, 0, null, 0, null, null)));
+												new Product(3, null, 0, 0, null, null, null),
+												new UserInfo(2, null, null, null, null, 0, null, 0, null, null)));
 		System.out.println(orderDetailDao.updateOrderByno(updateorderDetail_u_no));							  
 		
 		System.out.println("2.유저아이디를 이용한 상품수량 변경"); //유저 번호와 유저아이디를 찾아서 상품 수량 변경
-		OrderDetail updateorderDetail =	new OrderDetail(0,new Orders(0, null, null, 0, 7,
-										new Product(3, null, 0, 0, null, null, null),
-										new UserInfo(2,"itwill2", null, null, null, 0, null, 0, null, null)));
-		System.out.println(orderDetailDao.updateOrderByno(updateorderDetail));
-	
+		OrderDetail updateorderDetail_u_id =	new OrderDetail(0,new Orders(0, null, null, 0, 7,
+												new Product(3, null, 0, 0, null, null, null),
+												new UserInfo(0,null, null, null, null, 0, null, 0, null, null)));
+		System.out.println(orderDetailDao.updateOrderByno(updateorderDetail_u_id));
 		
 		
 		
-		
-//		System.out.println("1-1.상품 수량 변경"); //유저 아이디를 이용해서 상품 수량 변경
-//		OrderDetail updateorderDetail =	new OrderDetail(0,new Orders(0, null, null, 0, 7,
-//										new Product(3, null, 0, null, null, null),
-//										new UserInfo(3, itwill3, null, null, null, 0, null, 0, null, null)));
-//		System.out.println(orderDetailDao.update(updateorderDetail));	
-//		
-		
-		
+		System.out.println("3.유저아이디를 이용하여 주문목록 확인하기");
+		List<OrderDetail> orderDetailList = orderDetailDao.selectbyu_id("itwill3");
+		System.out.println(orderDetailList);
 
-		System.out.println("2.유저id로 결제상품 출력");
-		List<OrderDetail> orderDetailsList = orderDetailDao.selectbyid("itwill3");
-		System.out.println(">> " + orderDetailsList);
+		
 		
 		
 		
