d40ba5507cdb30a959ffbd8e08df4f86924176b6
Leejongchan99 <Leejongchan99>
2022-07-03 23:32:02 +0900

    ..

--- com/itwill/marketcoli/이종찬/OrderDaoTestMain.java
+++ com/itwill/marketcoli/이종찬/OrderDaoTestMain.java
@@ -13,11 +13,9 @@ public class OrderDaoTestMain {
       OrdersDao ordersDao = new OrdersDao();
    
       System.out.println("1.insert");
-     
-      Orders insertOrders =  new Orders(0, null, "입금완료", 3000, "버려주세요", 
-    		  null, 
-    		  null);
-    		  
+      Orders insertOrders = new Orders(0, null, "입금완료", 3000, "버려주세요", 
+				new UserInfo(0, null, null, null, null, 0, "강남", 0, null ,null), 
+				new Product(0, "사과",1 ,1000, null, null, null));
       System.out.println(ordersDao.insertOrders(insertOrders));
       
       System.out.println("2.update");
