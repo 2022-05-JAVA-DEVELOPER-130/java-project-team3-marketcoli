aa247a8f99fe758f27496b9cc005e2e32c95c54f
yoon <yoon@192.168.0.20>
2022-07-05 00:43:21 +0900

    ..

--- src/com/itwill/marketcoli/test/CartDaoTestMain.java
+++ src/com/itwill/marketcoli/test/CartDaoTestMain.java
@@ -15,17 +15,17 @@ public class CartDaoTestMain {
 		System.out.println(CartDao.insertCart(insertCart));
 		
 		System.out.println(">> 카트 번호로 카트 삭제");
-		System.out.println(CartDao.deleteCartNo(2));
+		System.out.println(CartDao.deleteCartNo(0));
 		
 		System.out.println(">> 유저 번호로 카트 삭제");
-		System.out.println(CartDao.deleteUserNo(2));
+		System.out.println(CartDao.deleteUserNo(0));
 		
 		System.out.println(">> 유저 번호로 카트에 담긴 상품 수량 변경");
 		Cart updateCart1 = new Cart(0,6,0,1);
 		System.out.println(CartDao.updateCartQty(updateCart1));
 
 		System.out.println(">> 유저 번호로 카트에 담긴 상품 변경");
-		Cart updateCart2 = new Cart(0,0,6,3);
+		Cart updateCart2 = new Cart(0,0,7,1);
 		System.out.println(CartDao.updateCartProduct(updateCart2));
 		
 		System.out.println(">> 카트 번호로 출력");
