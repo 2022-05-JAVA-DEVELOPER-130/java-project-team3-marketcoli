b949be558d923a8d2758e8402ee88c490d06746f
yoon <yoon@localhost>
2022-07-05 00:16:53 +0900

    ..

--- src/com/itwill/marketcoli/dao/CartSQL.java
+++ src/com/itwill/marketcoli/dao/CartSQL.java
@@ -1,21 +1,28 @@
 package com.itwill.marketcoli.dao;
 
+
 public class CartSQL {
 
 	//SQL 문장을 채워서 사용하세요
 	
 	public static final String CART_INSERT
-								= " ";
+								= "insert into cart values(cart_c_no_seq.nextval,?,?,?)";
 	
-	public static final String CART_UPDATE 
-								= " ";
+	public static final String CART_UPDATE_QTY
+								= "update cart set c_qty=? where u_no=?";
 	
-	public static final String CART_DELETE 
-								= " ";
+	public static final String CART_UPDATE_PRODUCT
+								= "update cart set p_no=? where u_no=?";
 	
-	public static final String CART_SELECT_BY_NO 
-								= " ";
+	public static final String CART_DELETE_CART_NO
+								= "delete from cart where c_no = ?";
+
+	public static final String CART_DELETE_USER_NO
+								= "delete from cart where u_no = ?";
+	
+	public static final String CART_SELECT_BY_USER_NO 
+								= "select * from cart where u_no = ?";
 	
 	public static final String CART_SELECT_ALL 
-								= " ";
+								= "select * from cart";
 }
--- src/com/itwill/marketcoli/test/CartDaoTestMain.java
+++ src/com/itwill/marketcoli/test/CartDaoTestMain.java
@@ -1,5 +1,41 @@
 package com.itwill.marketcoli.test;
 
+import java.util.List;
+
+import com.itwill.marketcoli.dao.CartDao;
+import com.itwill.marketcoli.dto.Cart;
+
 public class CartDaoTestMain {
+
+	public static void main(String[] args) throws Exception{
+		CartDao CartDao = new CartDao();
+		
+		System.out.println(">> 카트 담기");
+		Cart insertCart = new Cart(1,1,1,1);
+		System.out.println(CartDao.insertCart(insertCart));
+		
+		System.out.println(">> 카트 번호로 카트 삭제");
+		System.out.println(CartDao.deleteCartNo(2));
+		
+		System.out.println(">> 유저 번호로 카트 삭제");
+		System.out.println(CartDao.deleteUserNo(2));
+		
+		System.out.println(">> 유저 번호로 카트에 담긴 상품 수량 변경");
+		Cart updateCart1 = new Cart(0,6,0,1);
+		System.out.println(CartDao.updateCartQty(updateCart1));
+
+		System.out.println(">> 유저 번호로 카트에 담긴 상품 변경");
+		Cart updateCart2 = new Cart(0,0,6,3);
+		System.out.println(CartDao.updateCartProduct(updateCart2));
+		
+		System.out.println(">> 카트 번호로 출력");
+		Cart selectCartByNo = CartDao.selectCartByNo(1);
+		System.out.println(selectCartByNo);
+		
+		System.out.println(">> 카트 전체 출력");
+		List<Cart> cartList = CartDao.selectAll();
+		System.out.println(cartList);
+		
+	}
 
 }
