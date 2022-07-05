11417f5e02a037446996d1b933dfd818231b75f9
Harim <godtoether94@naver.com>
2022-07-05 19:29:16 +0900

    22.07.05 하림

--- src/com/itwill/marketcoli/service/OrderService.java
+++ src/com/itwill/marketcoli/service/OrderService.java
@@ -47,6 +47,13 @@ public class OrderService {
 	public int deleteOrderAllByOrderNo (int o_no) throws Exception {
 		return orderDao.deleteOrdersByOrderNo(o_no);
 	}
+	//유저아이디가 주문한 주문 전체삭제(u_id)
+	public int deleteOrderByUserId (String u_id)throws Exception{
+		return orderDao.deleteOrdersByUserId(u_id);
+	}
+	
+	
+	
 	
 	//주문 넣기
 	public int insertOrderOne (int p_no,int o_qty,String u_id ) throws Exception {
@@ -104,6 +111,7 @@ public class OrderService {
 		ArrayList<Orders> orderList = new ArrayList<Orders>();
 		
 		//for
+		
 		
 		return 0;
 	}
