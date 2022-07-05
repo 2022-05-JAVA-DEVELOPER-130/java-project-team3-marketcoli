4108eb18f644b4389073c7f8e9bd13064f2fdb2a
sojin_an <sojin_an@naver.com>
2022-07-05 14:42:58 +0900

    product class select문 외 정리진행

--- src/com/itwill/marketcoli/dao/ProductSQL.java
+++ src/com/itwill/marketcoli/dao/ProductSQL.java
@@ -4,15 +4,6 @@ public class ProductSQL {
 
 	//SQL 문장을 채워서 사용하세요
 
-	public static final String PRODUCT_INSERT = 
-			"insert into product values (product_p_no_seq.nextval,?,?,?,?,?)";
-
-	public static final String PRODUCT_UPDATE = 
-			"update product set p_name=?,p_price=?,p_exp=?,p_category_b=?,p_category_s=? where p_no=?";
-
-	public static final String PRODUCT_DELETE = 
-			"delete from product where p_no=?";
-
 	public static final String PRODUCT_SELECT_BY_NO = 
 			"select * from product where p_no=? ";
 
