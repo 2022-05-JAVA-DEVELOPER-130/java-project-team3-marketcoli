068e84cad40fc8578b464f855155285a3b7cff30
seoyoung <seoyoung310@gmail.com>
2022-07-05 19:57:37 +0900

    orderSevice

--- src/com/itwill/marketcoli/service/OrderService.java
+++ src/com/itwill/marketcoli/service/OrderService.java
@@ -13,19 +13,20 @@ import com.itwill.marketcoli.dto.OrderItem;
 import com.itwill.marketcoli.dto.Orders;
 import com.itwill.marketcoli.dto.Product;
 import com.itwill.marketcoli.dto.UserInfo;
+import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
 
 public class OrderService {
 	private OrdersDao orderDao;
 	private ProductDao productDao;
 	private CartDao cartDao;
 	private UserInfoDao userInfoDao;
-	
-	public OrderService() throws Exception{
-		orderDao=new OrdersDao();
-		productDao=new ProductDao();
-		cartDao=new CartDao();
+
+	public OrderService() throws Exception {
+		orderDao = new OrdersDao();
+		productDao = new ProductDao();
+		cartDao = new CartDao();
 	}
-	
+
 	/*public List<OrderItem> showOrderListByOrderNo (int o_no) throws Exception {
 		List<Orders> orderList = orderDao.selectAll();
 		Orders orderListByOrderNo = null;
@@ -44,22 +45,19 @@ public class OrderService {
 		return orderItemList;
 	}
 	*/
-	
-	
-	//주문 1개 전체 삭제
-	public int deleteOrderAllByOrderNo (int o_no) throws Exception {
+
+	// 주문 1개 전체 삭제
+	public int deleteOrderAllByOrderNo(int o_no) throws Exception {
 		return orderDao.deleteOrdersByOrderNo(o_no);
 	}
-	//유저아이디가 주문한 주문 전체삭제(u_id)
-	public int deleteOrderByUserId (String u_id)throws Exception{
+
+	// 유저아이디가 주문한 주문 전체삭제(u_id)
+	public int deleteOrderByUserId(String u_id) throws Exception {
 		return orderDao.deleteOrdersByUserId(u_id);
 	}
-	
-	
-	
-	
-	//주문 넣기
-	public int insertOrderOne (int p_no,int o_qty,String u_id ) throws Exception {
+
+	// 주문 넣기
+	public int insertOrderOne(int p_no, int o_qty, String u_id) throws Exception {
 		ProductDao selectProduct = new ProductDao();
 		Product product = selectProduct.selectByNo(p_no);
 		ArrayList<OrderItem> orderItemList = new ArrayList<OrderItem>();
@@ -69,15 +67,14 @@ public class OrderService {
 				new UserInfo(0, u_id, null, null, null, null, null, 0, null, null), orderItemList);
 		return orderDao.insertOrders(insertOrder);
 	}
-	
-	//주문을 넣은 상태에서, 추가하는 경우를 하고 싶었으나,
-	//동시에 2개를 주문을 넣는 경우
-	public int addOrders (int p1_no, int o1_qty, int p2_no, int o2_qty, String u_id ) throws Exception {
+
+	// 주문을 넣은 상태에서, 추가하는 경우를 하고 싶었으나,
+	// 동시에 2개를 주문을 넣는 경우
+	public int addOrders(int p1_no, int o1_qty, int p2_no, int o2_qty, String u_id) throws Exception {
 		ProductDao selectProduct = new ProductDao();
 		ArrayList<OrderItem> orderItemList = new ArrayList<OrderItem>();
-		
-		
-		//총가격 계산		-> (확인 필요)메소드로 넣는 작업, SQL문을 추가하여 작업
+
+		// 총가격 계산 -> (확인 필요)메소드로 넣는 작업, SQL문을 추가하여 작업
 		int totalPrice = 0;
 
 		ArrayList<Product> productList = new ArrayList<Product>();
@@ -85,16 +82,16 @@ public class OrderService {
 		Product product = selectProduct.selectByNo(p1_no);
 		productList.add(product);
 		orderItemList.add(new OrderItem(0, o1_qty, 0, product));
-		
+
 		product = selectProduct.selectByNo(p2_no);
 		productList.add(product);
-		
+
 		orderItemList.add(new OrderItem(0, o2_qty, 0, product));
-		
+
 		for (int i = 0; i < orderItemList.size(); i++) {
-			totalPrice += orderItemList.get(i).getOi_qty()*productList.get(i).getP_price();
+			totalPrice += orderItemList.get(i).getOi_qty() * productList.get(i).getP_price();
 		}
-		
+
 		Orders insertOrder = new Orders(0, null, totalPrice,
 				new UserInfo(0, u_id, null, null, null, null, null, 0, null, null), orderItemList);
 
@@ -102,36 +99,27 @@ public class OrderService {
 		return orderDao.insertOrders(insertOrder);
 
 	}
-	
-	
-	//cart에서 주문 -> cart에서 Order로 옮겨닮기
-	
-	public int cartToOrders (int u_no) throws Exception {
+
+	// cart에서 주문 -> cart에서 Order로 옮겨닮기
+	public Orders cartToOrders(int u_no) throws Exception {
 		ArrayList<Cart> cartList = new ArrayList<Cart>();
 		ArrayList<OrderItem> orderItemList = new ArrayList<OrderItem>();
-		
+
+		int oi_total_qty = 0;
+		int o_total_price = 0;
+
 		cartList = cartDao.selectCartByUserNo(u_no);
 		for (Cart cart : cartList) {
-		 orderItemList.add(new OrderItem(0, cart.getC_qty(), 0, productDao.selectByNo(cart.getP_no())));
+			Product product = productDao.selectByNo(cart.getP_no());
+			oi_total_qty = cart.getC_qty();
+			o_total_price += oi_total_qty * product.getP_price();
+			OrderItem orderItem = new OrderItem(0, oi_total_qty, 0, product);
+			orderItemList.add(orderItem);
 		}
+		Orders cartToOrders = new Orders(0, null, o_total_price, userInfoDao.selectByNo(u_no), orderItemList);
+		cartDao.deleteUserNo(u_no);
 
-		//Order cartToOrders =new OrderItem(0, u_no, u_no, null)
-		
-		
-		
-		
-		
-		
-		
-		
-		return 0;
+		return cartToOrders;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
