1073d9f35054e90bcacde2ac555e9278304e4e08
seoyoung <seoyoung310@gmail.com>
2022-07-05 10:14:25 +0900

    orderDao-Testmain 작업중 - 팀서버사용

--- src/com/itwill/marketcoli/dao/OrdersSQL.java
+++ src/com/itwill/marketcoli/dao/OrdersSQL.java
@@ -2,20 +2,21 @@ package com.itwill.marketcoli.dao;
 
 public class OrdersSQL {
 
-	//SQL 문장을 채워서 사용하세요
-	
-	public static final String ORDERS_INSERT
-								= " ";
-	
-	public static final String ORDERS_UPDATE 
-								= " ";
-	
-	public static final String ORDERS_DELETE 
-								= " ";
-	
-	public static final String ORDERS_SELECT_BY_NO 
-								= " ";
-	
-	public static final String ORDERS_SELECT_ALL 
-								= " ";
+	// SQL 문장을 채워서 사용하세요
+
+	public static final String ORDERS_INSERT = "insert into orders(o_no, o_date, o_price, u_id) values (orders_o_no_SEQ.nextval,sysdate, ?, ?)";
+	public static final String ORDERITEM_INSERT
+	//= "insert into order_item(oi_no, oi_qty, o_no, p_no) values(order_item_oi_no_SEQ.nextval, ? ,?, ?)";
+			= "insert into order_item(oi_no, oi_qty, o_no, p_no) values(order_item_oi_no_SEQ.nextval, ? ,orders_o_no_SEQ.currval, ?)";
+	/*
+	insert into orders(o_no,o_date,o_price,u_id) values (orders_o_no_SEQ.nextval,sysdate,10500,'itwill7');
+	insert into order_item(oi_no,oi_qty,o_no,p_no) values(order_item_oi_no_SEQ.nextval,1,orders_o_no_SEQ.currval,3);
+	*/
+	public static final String ORDERS_UPDATE = " ";
+
+	public static final String ORDERS_DELETE = " ";
+
+	public static final String ORDERS_SELECT_BY_NO = " ";
+
+	public static final String ORDERS_SELECT_ALL = " ";
 }
