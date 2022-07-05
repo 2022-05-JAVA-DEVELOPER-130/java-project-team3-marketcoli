44f3c46359ddd5bb620549f6f2c90edb369ed44f
seoyoung <seoyoung310@gmail.com>
2022-07-05 16:19:32 +0900

    OrderSevice 파일 생성

--- src/com/itwill/marketcoli/service/OrderService.java
+++ src/com/itwill/marketcoli/service/OrderService.java
@@ -1,5 +1,18 @@
 package com.itwill.marketcoli.service;
 
+import com.itwill.marketcoli.dao.CartDao;
+import com.itwill.marketcoli.dao.OrdersDao;
+import com.itwill.marketcoli.dao.ProductDao;
+
 public class OrderService {
+	private OrdersDao orderDao;
+	private ProductDao productDao;
+	private CartDao cartDao;
+	
+	public OrderService() throws Exception{
+		orderDao=new OrdersDao();
+		productDao=new ProductDao();
+		cartDao=new CartDao();
+	}
 
 }
