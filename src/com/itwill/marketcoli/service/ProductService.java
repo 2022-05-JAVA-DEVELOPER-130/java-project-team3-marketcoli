9a0123c4db7c335718b4fb7c2a3dea251f399416
sojin_an <sojin_an@naver.com>
2022-07-05 13:34:21 +0900

    ProductService 수정완료

--- src/com/itwill/marketcoli/service/ProductService.java
+++ src/com/itwill/marketcoli/service/ProductService.java
@@ -1,8 +1,26 @@
 package com.itwill.marketcoli.service;
 
-public class ProductService {
-	
+import java.util.List;
 
+import com.itwill.marketcoli.dao.ProductDao;
+import com.itwill.marketcoli.dto.Product;
+
+public class ProductService {
+	private ProductDao productDao;
+	
+	public ProductService() {
+		productDao = new ProductDao();
+	}
+		
+	//상품넘버로 상세페이지 불러오기
+	public Product productDetail(int p_no) throws Exception {
+		return productDao.selectByNo(p_no);
+	}
+	
+	//전체상품보기
+	public List<Product> productList() throws Exception {
+		return productDao.selsectAll();
+	}
 	
 
 }
--- src/com/itwill/marketcoli/test/ProductServiceTestMain.java
+++ src/com/itwill/marketcoli/test/ProductServiceTestMain.java
@@ -1,6 +1,16 @@
 package com.itwill.marketcoli.test;
 
-public class ProductServiceTestMain {
-	
+import com.itwill.marketcoli.service.ProductService;
 
+public class ProductServiceTestMain {
+
+	public static void main(String[] args) throws Exception {
+		ProductService productService = new ProductService();
+
+		System.out.println("1.상품넘버로 상세페이지 불러오기");
+		System.out.println(productService.productDetail(18));
+		
+		System.out.println("2.상품리스트 전체보기");
+		System.out.println(productService.productList());
+	}
 }
