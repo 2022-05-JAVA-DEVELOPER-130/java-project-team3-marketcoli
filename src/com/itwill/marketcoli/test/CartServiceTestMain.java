3036ec13da89a59012e3ffeaee36e93422641213
yooning92 <brv19@naver.com>
2022-07-05 14:21:49 +0900

    Cart (Service + Test) Files Push

--- src/com/itwill/marketcoli/test/CartServiceTestMain.java
+++ src/com/itwill/marketcoli/test/CartServiceTestMain.java
@@ -11,7 +11,7 @@ public class CartServiceTestMain {
 		System.out.println("1. add Cart");
 		rowCount = cartService.addCart(88,8,7);
 		System.out.println(">> " + rowCount);
-		/*
+		
 		System.out .println("2-1. update Cart_Qty < Cart_No");
 		rowCount =cartService.updateCartQty(99999, 7);
 		System.out.println(">> " + rowCount);
@@ -27,7 +27,6 @@ public class CartServiceTestMain {
 		System.out.println("3-2. delete Cart_User_No");
 		rowCount = cartService.deleteUserNo(7);
 		System.out.println(">> " + rowCount);
-		*/
 		
 		System.out.println("4-1. getCartNo");
 		System.out.println(cartService.getCartNo(7));
