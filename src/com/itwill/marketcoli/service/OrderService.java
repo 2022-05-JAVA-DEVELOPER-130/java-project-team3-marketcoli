aa1c535ebefc55f7c5c5fc73e5960c5376e44532
Harim <godtoether94@naver.com>
2022-07-05 20:56:09 +0900

    22.07.05 하림 service

--- src/com/itwill/marketcoli/service/OrderService.java
+++ src/com/itwill/marketcoli/service/OrderService.java
@@ -126,8 +126,8 @@ public class OrderService {
 	}
 
 	//주문번호 를 이용한 주문내역 전체 검색
-	public int selectOrderItemByno(int o_no)throws Exception {
-		return orderDao.findOrderByOi_No(o_no);
+	public String selectOrderItemByno(int o_no)throws Exception {
+		return null; //orderDao.findOrderByOi_No(o_no);
 		
 	}
 	
