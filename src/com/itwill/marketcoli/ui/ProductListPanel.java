9f227b341cfd8513d7febde0c8cc72747d8cd69e
sojin_an <sojin_an@naver.com>
2022-07-06 17:37:01 +0900

    ..

--- src/com/itwill/marketcoli/ui/ProductListPanel.java
+++ src/com/itwill/marketcoli/ui/ProductListPanel.java
@@ -4,104 +4,124 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.util.List;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JTabbedPane;
+
+import com.itwill.marketcoli.dto.Product;
+import com.itwill.marketcoli.service.ProductService;
+
 import javax.swing.JScrollPane;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 
 public class ProductListPanel extends JPanel {
-	private JPanel panel_4;
-	private JPanel panel_2;
-	private JScrollPane scrollPane;
-	private JPanel panel;
-	private JTabbedPane tabbedPane;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel;
-	private JPanel panel_1;
-	private JLabel lblNewLabel_4;
-	private JLabel lblNewLabel_5;
-	private JLabel lblNewLabel_6;
-	private JLabel lblNewLabel_7;
-	private JLabel lblNewLabel_8;
-	private JLabel lblNewLabel_9;
+	
+	private ProductService productService;
+
+	private JPanel pItemPanel;
+	private JPanel pItemListPanel;
+	private JScrollPane pScrollPane;
+	private JPanel pListPanel;
+	private JTabbedPane productTabbedPane;
+	private JLabel pLabel_3;
+	private JLabel pLabel_2;
+	private JLabel pLabel;
+	private JLabel pLabel_4;
 
 	/**
 	 * Create the panel.
 	 */
 	public ProductListPanel() {
 		setLayout(new BorderLayout(390, 680));
-		
-		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		add(tabbedPane, BorderLayout.CENTER);
-		
-		panel = new JPanel();
-		tabbedPane.addTab("전체상품보기", null, panel, null);
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		scrollPane = new JScrollPane();
-		panel.add(scrollPane, BorderLayout.CENTER);
-		
-		panel_2 = new JPanel();
-		panel_2.setPreferredSize(new Dimension(10, 800));
-		scrollPane.setViewportView(panel_2);
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		panel_4 = new JPanel();
-		panel_4.setBackground(new Color(230, 230, 250));
-		panel_4.setPreferredSize(new Dimension(390, 155));
-		panel_2.add(panel_4);
-		panel_4.setLayout(null);
-		
-		lblNewLabel = new JLabel("사진");
-		lblNewLabel.setBounds(12, 10, 135, 135);
-		panel_4.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("상품명");
-		lblNewLabel_1.setBounds(159, 10, 219, 15);
-		panel_4.add(lblNewLabel_1);
-		
-		lblNewLabel_2 = new JLabel("상품가격");
-		lblNewLabel_2.setBounds(159, 50, 219, 15);
-		panel_4.add(lblNewLabel_2);
-		
-		lblNewLabel_3 = new JLabel("상품설명");
-		lblNewLabel_3.setBounds(159, 85, 219, 60);
-		panel_4.add(lblNewLabel_3);
-		
-		panel_1 = new JPanel();
-		tabbedPane.addTab("상세페이지", null, panel_1, null);
-		panel_1.setLayout(null);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(12, 10, 310, 310);
-		panel_1.add(panel_3);
-		
-		lblNewLabel_4 = new JLabel("상품명");
-		lblNewLabel_4.setBounds(12, 352, 57, 15);
-		panel_1.add(lblNewLabel_4);
-		
-		lblNewLabel_5 = new JLabel("New label");
-		lblNewLabel_5.setBounds(81, 352, 241, 15);
-		panel_1.add(lblNewLabel_5);
-		
-		lblNewLabel_6 = new JLabel("상품가격");
-		lblNewLabel_6.setBounds(12, 390, 57, 15);
-		panel_1.add(lblNewLabel_6);
-		
-		lblNewLabel_7 = new JLabel("New label");
-		lblNewLabel_7.setBounds(81, 390, 241, 15);
-		panel_1.add(lblNewLabel_7);
-		
-		lblNewLabel_8 = new JLabel("상품설명");
-		lblNewLabel_8.setBounds(12, 430, 57, 15);
-		panel_1.add(lblNewLabel_8);
-		
-		lblNewLabel_9 = new JLabel("New label");
-		lblNewLabel_9.setBounds(81, 430, 241, 15);
-		panel_1.add(lblNewLabel_9);
 
+		productTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		add(productTabbedPane, BorderLayout.CENTER);
+
+		pListPanel = new JPanel();
+		productTabbedPane.addTab("전체상품보기", null, pListPanel, null);
+		pListPanel.setLayout(new BorderLayout(0, 0));
+
+		pScrollPane = new JScrollPane();
+		pListPanel.add(pScrollPane, BorderLayout.CENTER);
+		/**********************************************************/
+		pItemListPanel = new JPanel();
+		pItemListPanel.setPreferredSize(new Dimension(10, 800));
+		pScrollPane.setViewportView(pItemListPanel);
+		pItemListPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		pItemPanel = new JPanel();
+		pItemPanel.setBackground(new Color(230, 230, 250));
+		pItemPanel.setPreferredSize(new Dimension(390, 155));
+		pItemListPanel.add(pItemPanel);
+		pItemPanel.setLayout(null);
+
+		pLabel = new JLabel("그림");
+		pLabel.setIcon(new ImageIcon(ProductListPanel.class.getResource("/images/listProduct/체리.jpg")));
+		pLabel.setBounds(12, 10, 135, 135);
+		pItemPanel.add(pLabel);
+
+		pLabel_4 = new JLabel("상품명");
+		pLabel_4.setBounds(159, 20, 219, 15);
+		pItemPanel.add(pLabel_4);
+
+		pLabel_2 = new JLabel("가격");
+		pLabel_2.setBounds(159, 50, 219, 15);
+		pItemPanel.add(pLabel_2);
+
+		pLabel_3 = new JLabel("설명");
+		pLabel_3.setBounds(159, 75, 219, 60);
+		pItemPanel.add(pLabel_3);
+		/**********************************************************/
+
+		productService = new ProductService();
+		try {
+			productListDisplay();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	/**************** productListDisplay 메소드 ******************/
+	public void productListDisplay() throws Exception {
+		pItemListPanel.removeAll();
+		List<Product> productList = productService.productList();
+		for (int i = 0; i < productList.size(); i++) {
+			Product product = productList.get(i);
+
+			pItemListPanel = new JPanel();
+			pItemListPanel.setPreferredSize(new Dimension(10, 800));
+			pScrollPane.setViewportView(pItemListPanel);
+			pItemListPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+			pItemPanel = new JPanel();
+			pItemPanel.setBackground(new Color(230, 230, 250));
+			pItemPanel.setPreferredSize(new Dimension(390, 155));
+			pItemListPanel.add(pItemPanel);
+			pItemPanel.setLayout(null);
+
+			pLabel = new JLabel("");
+			pLabel.setIcon(new ImageIcon(ProductListPanel.class.getResource("/images/listProduct/"+product.getP_name()+".jpg")));
+			pLabel.setBounds(12, 10, 135, 135);
+			pItemPanel.add(pLabel);
+
+			JLabel pLabel_1 = new JLabel(product.getP_name());
+			pLabel_1.setBounds(159, 20, 219, 15);
+			pItemPanel.add(pLabel_1);
+
+			pLabel_2 = new JLabel(product.getP_price()+"");
+			pLabel_2.setBounds(159, 50, 219, 15);
+			pItemPanel.add(pLabel_2);
+
+			pLabel_3 = new JLabel(product.getP_exp());
+			pLabel_3.setBounds(159, 75, 219, 60);
+			pItemPanel.add(pLabel_3);
+
+		}
 	}
 }
